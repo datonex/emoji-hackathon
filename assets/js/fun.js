6ef350c4a9d9a78bf4371a6fc4b20f885a90970b
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-07-16 13:43:25 +0000

    Add fun.js file

---
+++ assets/js/fun.js
@@ -0,0 +1,52 @@
+let questions = [
+{
+    question: 'Which festival does 🎅 represent?',
+    option: ['Easter','Christmas','Halloween'],
+    answer: 2,
+},
+{
+    question: 'Which event does 🌱 represent?',
+    options: ['Earth day','Globe day','St Patricks day'],
+    answer: 1,
+},
+{
+    question: 'Which event does 🏅 represent?',
+    options: ['Volleyball','Tennis', 'Olympics'],
+    answer: 3, 
+},
+{
+    question: 'Which festival does 🎃 represent?',
+    options: ['Halloweeen','New years eve','Independence Day'],
+    answer: 1,
+},
+{
+    question: 'Which festival does ⚫ represent?',
+    options: ['Hannukah','Black friday','Chinese New year'],
+    answer: 2,
+},
+{
+    question: 'Which event does 📖 represent?',
+    options: ['World book day','Graduation','Womens day'],
+    answer: 1,
+},
+{
+    question: 'Which event does 👩‍🍼 represent?',
+    options: ['Mothers day','Fathers day','Childrens day'],
+    answer: 1,
+},
+{
+    question: 'Which event does 🐉 represent?',
+    options: ['Chinese New Year','New Year','emoji day'],
+    answer: 1,
+},
+{
+    question: 'Which event does ☘️ represent?',
+    options: ['Earth day', 'St Patricks day','emoji day'],
+    answer: 2,
+},
+{
+    question: 'Which event does ❤️ represent?',
+    options: ['Thanks giving','Veterans day','valentines day'],
+    answer: 3,
+},
+]
