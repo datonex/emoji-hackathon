1a10dda1a0124ea1d0d00c2e2a0bb1429495866a
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-07-17 11:05:40 +0000

    Display quiz question text on screen

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,13 @@
+let counter = 0;
 window.addEventListener("load", function (event) {
 
     console.log(questions);
+    document.getElementById("quiz").innerHTML = questions[0].question;
 });
+function next() {
+    //Next button should not be displayed on the end of the questions array
+    counter++;
+    console.log(questions[counter]);
+    console.log(questions[counter].question);
+    document.getElementById("quiz").innerHTML = questions[counter].question;
+}
