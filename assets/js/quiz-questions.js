d523b14474bd86c0ab3bb5b102e4c2fce3d71a45
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-07-17 21:45:09 +0000

    Add flash message

--- assets/js/quiz-questions.js
+++ assets/js/quiz-questions.js
@@ -2,51 +2,51 @@ let questions = [
 {
     question: 'Which festival does &#127877 represent?',
     options: ['Easter','Christmas','Halloween'],
-    answer: 2,
+    answer: 'Christmas',
 },
 {
     question: 'Which event does &#127793 represent?',
     options: ['Earth day','Globe day','St Patricks day'],
-    answer: 1,
+    answer: 'Earth day',
 },
 {
     question: 'Which event does &#127941 represent?',
     options: ['Volleyball','Tennis', 'Olympics'],
-    answer: 3, 
+    answer: 'Olympics', 
 },
 {
     question: 'Which festival does &#127875 represent?',
     options: ['Halloweeen','New Years Eve','Independence Day'],
-    answer: 1,
+    answer: 'Halloweeen',
 },
 {
     question: 'Which festival does &#9899 represent?',
     options: ['Hannukah','Black friday','Chinese New year'],
-    answer: 2,
+    answer: 'Black friday',
 },
 {
     question: 'Which event does &#128214 represent?',
     options: ['World Book day','Graduation','Womens day'],
-    answer: 1,
+    answer: 'World Book day',
 },
 {
     question: 'Which event does &#128105 represent?',
     options: ['Mothers day','Fathers day','Childrens day'],
-    answer: 1,
+    answer: 'Mothers day',
 },
 {
     question: 'Which event does &#128009 represent?',
     options: ['Chinese New Year','New Year','Emoji day'],
-    answer: 1,
+    answer: 'Chinese New Year',
 },
 {
     question: 'Which event does &#9752 represent?',
     options: ['Earth day', 'St Patricks day','Emoji day'],
-    answer: 2,
+    answer: 'St Patricks day',
 },
 {
     question: 'Which event does &#10084 represent?',
     options: ['Thanks Giving','Veterans day','Valentines day'],
-    answer: 3,
+    answer: 'Valentines day',
 },
 ]
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,24 +2,52 @@ let counter = 0;
 window.addEventListener("load", function (event) {
 
     console.log(questions);
-    document.getElementById("quiz").innerHTML = questions[0].question;
+    document.getElementById("question").innerHTML = questions[0].question;
+    
     document.getElementById("answer1").innerHTML = questions[0].options[0];
+    document.getElementById("inlineRadio1").value = questions[0].options[0];
     document.getElementById("answer2").innerHTML = questions[0].options[1];
+    document.getElementById("inlineRadio2").value = questions[0].options[1];
     document.getElementById("answer3").innerHTML = questions[0].options[2];
+    document.getElementById("inlineRadio3").value = questions[0].options[2];
 });
 
 function next() {
     //Next button should not be displayed on the end of the questions array
+
+    
+    if(counter+1 == questions.length){
+        return false;
+    }
     counter++;
     console.log(questions[counter]);
     console.log(questions[counter].question);
-    document.getElementById("quiz").innerHTML = questions[counter].question;
+    document.getElementById("question").innerHTML = questions[counter].question;
     document.getElementById("answer1").innerHTML = questions[counter].options[0];
+    document.getElementById("inlineRadio1").value = questions[counter].options[0];
     document.getElementById("answer2").innerHTML = questions[counter].options[1];
+    document.getElementById("inlineRadio2").value = questions[counter].options[1];
     document.getElementById("answer3").innerHTML = questions[counter].options[2];
+    document.getElementById("inlineRadio3").value = questions[counter].options[2];
+
+    document.getElementById("result-message").innerHTML = "";
+    document.getElementById("inlineRadio1").checked = false;
+    document.getElementById("inlineRadio2").checked = false;
+    document.getElementById("inlineRadio3").checked = false;
+
 }
 
 //this could be done by DOM manipulation too.. 
-function checkAnswer() {
-    console.log('checkAnswer called');
+function checkAnswer(event) {
+    //console.log('checkAnswer called');
+    // if(document.getElementById("quiz").checked)
+    //     document.getElementById('').innerHTML = "Bravo";
+    console.log(event.value);
+    console.log(questions[counter].answer)
+    if(event.value == questions[counter].answer ) {
+        document.getElementById("result-message").innerHTML = "Your answer is correct";
+    }else{
+        document.getElementById("result-message").innerHTML = "Your answer is wrong";
+    }
+
 }
